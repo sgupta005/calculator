a66a28a8cc08c20fb322fdd2bc1ad6f03f7262e2
Shivam Gupta <shivamgupta02005@gmail.com>
2023-10-29 00:59:20 +0530

    Made calculator perform +,-,/,* operations

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 function add(num1,num2){
-    return num1+num2;
+    return Number(num1)+Number(num2);
 }
 function subtract(num1,num2){
-    return num1-num2;
+    return Number(num1)-Number(num2);
 }
 function multiply(num1,num2){
-    return num1*num2;
+    return Number(num1)*Number(num2);
 }
 function divide(num1,num2){
-    return num1/num2
+    return Number(num1)/Number(num2);
 }
 function remainder(num1,num2){
-    return num1%num2;
+    return Number(num1)%Number(num2);
 }
 
 function operate(num1,num2,operator){
@@ -30,3 +30,45 @@ function operate(num1,num2,operator){
     return result;
 }
 
+screen = document.querySelector('#screen-input');
+all_clear_button = document.querySelector('.all-clear');
+all_clear_button.addEventListener('click', ()=>screen.value = '');
+
+const digits = document.querySelectorAll('.digits');
+digits.forEach(digit=> digit.addEventListener('click', ()=>screen.value += digit.textContent ));
+
+const operators = document.querySelectorAll('.operators');
+operators.forEach(operator => operator.addEventListener('click', operatorClick));
+
+let num1 = null;
+let num2 = null;
+let operator = null;
+const operatorRegex = /[+\-/*%]/;
+function operatorClick(event){
+    if (operatorRegex.test(screen.value)){
+        values = screen.value.split(operatorRegex);
+        num2 = values[1];
+        console.log('running if')
+        console.log(num1,num2,operator);
+        result = operate(num1,num2,operator);
+        console.log(result);
+        num1 = result;
+        operator = event.target.textContent;
+        screen.value = result + operator;
+        num2 = null;   
+        console.log(num1,num2,operator);
+    }else{
+        console.log('running else');
+        num1 = screen.value;
+        console.log(num1);
+        operator = event.target.textContent;
+        console.log(operator);
+        screen.value+=event.target.textContent;
+    }
+}
+
+// equals_button = document.querySelector('.equals');
+// equals_button.addEventListener('click', ()=>{
+//     result = operate(num1,num2,operator);
+//     console.log(result);
+// })
